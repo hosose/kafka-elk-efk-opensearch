e2d52ea97c4c955a29935ecd69383d13fe30a395
hosose <hosose22@gmail.com>
2026-08-28 14:17:57 +0900

    feat: add IAM roles and policies for Firehose and Vector logging services

--- infra/iam-vector.tf
+++ infra/iam-vector.tf
@@ -1,10 +1,28 @@
-# Firehose -> OpenSearch 전달 오류를 확인하기 위한 CloudWatch Logs
-resource "aws_cloudwatch_log_group" "firehose" {
-  name              = local.firehose_log_group
-  retention_in_days = 7
+# 로컬 Vector가 Firehose에 데이터를 넣기 위한 최소 권한 정책
+resource "aws_iam_policy" "vector_firehose_put" {
+  name        = "${var.project_name}-vector-put"
+  description = "Allow local Vector to put records into the lab Firehose stream"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "firehose:PutRecord",
+          "firehose:PutRecordBatch",
+          "firehose:DescribeDeliveryStream"
+        ]
+        Resource = aws_kinesis_firehose_delivery_stream.opensearch.arn
+      }
+    ]
+  })
 }
 
-resource "aws_cloudwatch_log_stream" "firehose" {
-  name           = local.firehose_log_stream
-  log_group_name = aws_cloudwatch_log_group.firehose.name
+# 기존 IAM User 이름을 지정한 경우에만 위 정책을 자동으로 연결한다.
+resource "aws_iam_user_policy_attachment" "vector" {
+  count = var.vector_iam_user_name == "" ? 0 : 1
+
+  user       = var.vector_iam_user_name
+  policy_arn = aws_iam_policy.vector_firehose_put.arn
 }
